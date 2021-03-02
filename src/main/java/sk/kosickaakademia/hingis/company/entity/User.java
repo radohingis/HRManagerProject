767cc3c7946da5f84620617f479a1fb13dabef06
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-02 10:44:57 +0100

    created enumerators for Gender variable in User entity

--- src/main/java/sk/kosickaakademia/hingis/company/entity/User.java
+++ src/main/java/sk/kosickaakademia/hingis/company/entity/User.java
@@ -1,21 +1,43 @@
 package sk.kosickaakademia.hingis.company.entity;
 
+import sk.kosickaakademia.hingis.company.enumerator.Gender;
+
 public class User {
     private int id;
     private String fname;
     private String lname;
     private int age;
-    private boolean gender;
+    private Gender gender;
 
-    public User(String fname, String lname, int age, boolean gender) {
+    public User(String fname, String lname, int age, int gender) {
         this.fname = fname;
         this.lname = lname;
         this.age = age;
-        this.gender = gender;
+        this.gender = gender == 0 ? Gender.MALE : gender == 1 ? Gender.FEMALE : Gender.OTHER;
     }
 
-    public User(int id, String fname, String lname, int age, boolean gender) {
+    public User(int id, String fname, String lname, int age, int gender) {
         this(fname, lname, age, gender);
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
---
+++ src/main/java/sk/kosickaakademia/hingis/company/enumerator/Gender.java
@@ -0,0 +1,5 @@
+package sk.kosickaakademia.hingis.company.enumerator;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
