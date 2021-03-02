c8c10a7ef4a79b2b4898b3770b215723aa17571b
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-02 10:20:31 +0100

    added private vars to User

--- src/main/java/sk/kosickaakademia/hingis/company/entity/User.java
+++ src/main/java/sk/kosickaakademia/hingis/company/entity/User.java
@@ -1,4 +1,24 @@
 package sk.kosickaakademia.hingis.company.entity;
 
 public class User {
+    private int id;
+    private String fname;
+    private String lname;
+    private int age;
+    private boolean gender;
+
+    public User(String fname, String lname, int age, boolean gender) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public User(int id, String fname, String lname, int age, boolean gender) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.gender = gender;
+    }
 }
