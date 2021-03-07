659ac82077b41844dd94ae4e2f91e8c15c07abe4
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-07 18:00:27 +0100

    added parseToJson util method

--- src/main/java/sk/kosickaakademia/hingis/company/util/Util.java
+++ src/main/java/sk/kosickaakademia/hingis/company/util/Util.java
@@ -1,5 +1,10 @@
 package sk.kosickaakademia.hingis.company.util;
 
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import sk.kosickaakademia.hingis.company.entity.User;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,5 +17,12 @@ public class Util {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         return formatter.format(dateNow);
+    }
+
+    public String parseToJSON(User user) {
+        JsonObject data = new JsonObject();
+        data.addProperty("date", getCurrentDateTime());
+        data.add("user", new Gson().toJsonTree(user));
+        return data.toString();
     }
 }
