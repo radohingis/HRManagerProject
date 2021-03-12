ddcfba10b996606ae002e96a29811aef39769194
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-12 10:15:35 +0100

    Repaired getUserById (@GetMapping) in MainController)

--- src/main/java/sk/kosickaakademia/hingis/company/controller/MainController.java
+++ src/main/java/sk/kosickaakademia/hingis/company/controller/MainController.java
@@ -1,9 +1,11 @@
 package sk.kosickaakademia.hingis.company.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.JsonObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import sk.kosickaakademia.hingis.company.database.SQL;
+import sk.kosickaakademia.hingis.company.entity.User;
 import sk.kosickaakademia.hingis.company.util.Util;
 
 @RestController
@@ -11,8 +13,16 @@ public class MainController {
 
 
     @GetMapping("/user/{id}")
-    public String getUserById(@PathVariable int id) {
-        return new Util().parseToJSON(new SQL().getUserById(id));
+    public ResponseEntity<String> getUserById(@PathVariable int id) {
+        User user = new SQL().getUserById(id);
+        if(user != null) {
+            String userJson = new Util().parseToJSON(user);
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(userJson);
+        }
+        JsonObject error = new JsonObject();
+        error.addProperty("message", "user not found, bad request");
+        error.addProperty("date", new Util().getCurrentDateTime());
+        return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(error.toString());
     }
 
     @GetMapping("/users")
