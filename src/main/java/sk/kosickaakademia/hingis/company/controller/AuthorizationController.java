1c2c058716be9dca211527286b841796918a088b
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-21 11:14:16 +0100

    added secret method to AuthorizationController

--- src/main/java/sk/kosickaakademia/hingis/company/controller/AuthorizationController.java
+++ src/main/java/sk/kosickaakademia/hingis/company/controller/AuthorizationController.java
@@ -6,18 +6,22 @@ import com.google.gson.JsonObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.kosickaakademia.hingis.company.util.Util;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthorizationController {
 
+    private Map<String, String> tokens = new HashMap<>();
+
     private final String PASSWORD = "radovesilneheslo";
 
     @PostMapping("/login")
-    public ResponseEntity<String> login (@RequestBody String auth_data){
+    public ResponseEntity<String>
+    login (@RequestBody String auth_data) {
 
         JsonObject json_auth_data
                     = new Gson()
@@ -40,6 +44,9 @@ public class AuthorizationController {
 
                 jsonObject.addProperty("login", login);
 
+                tokens.put("login", login);
+                tokens.put("token", token);
+
                 jsonObject.addProperty("token", "Bearer " + token);
 
                 return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(jsonObject.toString());
@@ -47,6 +54,22 @@ public class AuthorizationController {
             else {
                 return new ResponseEntity<>("Wrong password", HttpStatus.UNAUTHORIZED);
             }
+    }
+
+    @GetMapping("/secret")
+    public String secret(@RequestHeader("token") String header){
+
+        String token = header.substring(7);
+
+        System.out.println(token);
+
+        for (Map.Entry<String, String> entry : tokens.entrySet()) {
+            System.out.println(entry.getValue());
+            if(entry.getValue().equals(token)){
+                return "secret";
+            }
+        }
+        return "Invalid token";
     }
 
 }
